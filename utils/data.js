21f74fb8e04ee1211ff9b567ccdde9948cbc1b84
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-06-02 09:41:31 +0100

    made changes

--- utils/data.js
+++ utils/data.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 export const originURL =
   process.env.NODE_ENV === "production"
-    ? "https://bmw-naija.web.app"
+    ? "https://bmw-naija.web.app/"
     : "http://localhost:3000";
